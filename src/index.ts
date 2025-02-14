fe9f93c9d2656def48edfe9ca6cf9fffb8e7d53d
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-02-14 12:53:27 +0000

    feat: add Firebase and Genkit AI integrations to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,22 @@ import "dotenv/config";
 import * as cheerio from "cheerio";
 
 import { genkit, z } from "genkit";
-import { gpt4o, openAI } from "genkitx-openai";
+import { gpt4o,  textEmbeddingAda002, openAI } from "genkitx-openai";
 
 import { logger } from "genkit/logging";
 logger.setLogLevel("debug");
+
+import { retrieve } from "@genkit-ai/ai";
+import { configureGenkit } from "@genkit-ai/core";
+import { firebaseAuth } from "@genkit-ai/firebase/auth";
+import { onFlow } from "@genkit-ai/firebase/functions";
+
+import { defineFirestoreRetriever, firebase } from "@genkit-ai/firebase";
+import { getFirestore } from "firebase-admin/firestore";
+import { defineDotprompt } from '@genkit-ai/dotprompt';
+import { dotprompt } from '@genkit-ai/dotprompt';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 const ai = genkit({
   plugins: [openAI({ apiKey: process.env.OPENAI_API_KEY })],
